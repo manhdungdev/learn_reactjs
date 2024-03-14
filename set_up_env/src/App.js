d39f534143ceb87f838f1bedae49b5376fcf925c
Hoang Manh Dung <manhdung18102003@gmail.com>
2024-03-14 16:45:26 +0700

    Understand JSX

--- set_up_env/src/App.js
+++ set_up_env/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const comment = '<script>console.log("Hehehee")</script>';
+
 function App() {
   return (
     <div className='App'>
-    <h1>Created by Dung HM</h1>
+      <h1>Created by Dung HM</h1>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{comment}</p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
---
+++ set_up_env/src/header.jsx
@@ -0,0 +1,23 @@
+const lastName = 'Dung HM';
+
+const sum = (a, b) => a + b;
+const element = (
+  <h1>
+    Hello world, I am {lastName}. I am {sum(20, 1)}
+  </h1>
+);
+
+const element2 = (
+  <a href='google.com' tabIndex='11' className='helo'>
+    Google
+  </a>
+);
+const element3 = <h3 />;
+
+const element4 = <img src='' />;
+
+const element5 = <input type='text' />;
+
+const element6 = '<script>console.log("Hehehee")</script>';
+
+const element7 = <h1 className='meet'>Hello world</h1>;
