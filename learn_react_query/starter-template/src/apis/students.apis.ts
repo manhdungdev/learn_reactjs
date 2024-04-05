687f36f1e4ea89fb10d337d48a197a1c1e81d784
Hoang Manh Dung <manhdung18102003@gmail.com>
2024-04-05 09:35:51 +0700

    learn react_query

---
+++ learn_react_query/starter-template/src/apis/students.apis.ts
@@ -0,0 +1,12 @@
+import { Students } from 'types/Student.type'
+import http from 'utils/http'
+
+export const getStudents = (page: number | string, limit: number | string) => {
+  return http.get<Students>('students', {
+    params: {
+      _page: page,
+      _limit: limit
+    }
+  })
+}
+
